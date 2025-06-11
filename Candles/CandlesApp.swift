c7f18af155d1ee5e6355468db25f1cf0ee3089b3
Jaspreet Malak <jass.malak13@gmail.com>
2025-06-10 17:31:28 -0700

    Initial Commit

---
+++ Candles/CandlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandlesApp.swift
+//  Candles
+//
+//  Created by Jaspreet Malak on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CandlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
